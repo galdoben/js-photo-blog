3af2aafebc5c088c6feac75375d0a2a0d4956c85
galdoben <galdobenedetta@gmail.com>
2024-12-06 17:17:32 +0100

    fix:end-ex

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 const endPoint = 'https://jsonplaceholder.typicode.com/photos?_limit=6'
 
 const containerCard = document.querySelector('.row');
-
+const overPlan = document.getElementById('overlay');
+const button = document.querySelector('.btn');
 
 axios.get(endPoint)
 .then(response => {
  console.log(response.data)
 response.data.forEach(album => printCard (album))
+
+document.querySelectorAll('.card-img-top').forEach((card) => {
+    card.addEventListener("click", clickImg);
+    //console.log(card)//
+})
 })
 
+button.addEventListener("click", clickBut);
+function clickBut() {
+ overPlan.style.display = 'none';
+}
 
+function clickImg(event) {
+overPlan.style.display = 'block';
+const url = event.target.src;
+const image = document.querySelector('#overlay img');
+image.src = url
+
+console.log(url)
+
+
+}
+
+
+//function on (){
+//    overPlan.style.display = 'block';//
+//}
+//function off (){//
+//   ;//
+//}//
 
 
 
